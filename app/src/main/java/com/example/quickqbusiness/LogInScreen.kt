382a84c5df71f317903de282b27ccec96549d051
Atharv-15 <atharv.work01@gmail.com>
2024-08-07 17:03:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quickqbusiness/LogInScreen.kt
@@ -0,0 +1,135 @@
+package com.example.quickqbusiness
+
+import android.content.Intent
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import com.example.quickqbusiness.ui.theme.QuickQBusinessTheme
+
+@Composable
+fun LogInScreen() {
+
+    val context = LocalContext.current
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.queue), contentDescription = "Log In Image",
+            modifier = Modifier
+                .size(300.dp)
+        )
+        
+        Text(text = "Hello", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier
+            .height(4.dp)
+        )
+
+        Text(text = "Log In to your account")
+
+        Spacer(modifier = Modifier
+            .height(16.dp)
+        )
+
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = {
+            Text(text = "Email address")
+        })
+
+        Spacer(modifier = Modifier
+            .height(16.dp)
+        )
+
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
+            Text(text = "Password")
+        }, visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier
+            .height(16.dp)
+        )
+
+        Button(onClick = {
+            Log.i("Credentials", "Email: $email, Password: $password")
+            context.startActivity(Intent(context, HomePage::class.java))
+        }) {
+            Text(text = "Log In")
+        }
+
+        Spacer(modifier = Modifier
+            .height(16.dp)
+        )
+
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Forgot Password?")
+        }
+
+        Spacer(modifier = Modifier
+            .height(32.dp)
+        )
+
+        Text(text = "Or sign in with")
+
+        Spacer(modifier = Modifier
+            .height(32.dp)
+        )
+
+        Image(painter = painterResource(id = R.drawable.google),
+            contentDescription = "Google",
+            modifier = Modifier
+                .size(60.dp)
+                .clickable { /*TODO*/ }
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LogInPreview() {
+    QuickQBusinessTheme {
+        LogInScreen()
+    }
+}
